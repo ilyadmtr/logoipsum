4b032b6cb8b220ce5574b48a6749cb2f9ecb103e
ilyadmtr <ilyadmytrenko@gmail.com>
2021-11-22 15:14:35 +0200

    amendments

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
 document.querySelector('.burger').addEventListener('click', function(){
-  document.querySelector('.burger span').classList.toggle('active');
+  document.querySelector('.burger span').classList.toggle('active_b');
   document.querySelector('.menu').classList.toggle("animate");
 })
